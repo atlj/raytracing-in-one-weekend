5de5731d5bec3aa9736584b24f7ac5506d5b6b8e
Burak <md.burak.guner@gmail.com>
2022-10-01 17:04:14 +0300

    feat: indicate the progress

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <fstream>
 
+using std::cout;
+using std::chrono::steady_clock;
+
 int main()
 {
+    auto programStartTime = steady_clock::now();
     const int width = 256;
     const int height = 256;
 
@@ -13,19 +17,22 @@ int main()
     image << "P3\n"
           << width << " " << height << "\n255\n";
 
-    for (int y = height - 1; y >= 0; --y)
+    for (int y = 0; y < height; ++y)
     {
         for (int x = 0; x < width; ++x)
         {
             double r = double(x) / (width - 1);
             double g = double(y) / (height - 1);
-            double b = 0.90;
+            double b = 0.5;
 
             int ir = static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);
             image << ir << " " << ig << " " << ib << "\n";
         }
+        cout << (float(y) / float(height - 1)) * 100.0 << "%\n"
+             << std::flush;
     }
     image.close();
+    cout << "Done in " << std::chrono::duration_cast<std::chrono::milliseconds>(steady_clock::now() - programStartTime).count() << "ms" << std::endl;
 }
