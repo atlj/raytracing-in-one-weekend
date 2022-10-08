681a944e8d75d64fbea501369fde8acb770518aa
Burak <md.burak.guner@gmail.com>
2022-10-08 18:12:09 +0300

    feat: antialiasing

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "color.h"
 #include "sphere.h"
 #include "hittable_list.h"
+#include "camera.h"
 
 using std::cout;
 using std::chrono::steady_clock;
@@ -32,6 +33,7 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 50;
     std::ofstream image;
     image.open("image.ppm");
 
@@ -41,14 +43,7 @@ int main()
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    camera cam;
 
     // Render
     image << "P3\n"
@@ -58,12 +53,17 @@ int main()
     {
         for (int x = 0; x < image_width; ++x)
         {
-            auto u = double(x) / (image_width - 1);
-            auto v = double(y) / (image_height - 1);
-            ray r = ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color(0, 0, 0);
 
-            color pixel_color = ray_color(r, world);
-            write_color(image, pixel_color);
+            for (int sample_count = 0; sample_count < samples_per_pixel; ++sample_count)
+            {
+                auto u = (x + random_double()) / (image_width - 1);
+                auto v = (y + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+
+            write_color(image, pixel_color, samples_per_pixel);
         }
         cout << (float(image_height - y) / float(image_height)) * 100.0 << "%\n"
              << std::flush;
